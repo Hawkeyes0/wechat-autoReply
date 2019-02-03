c1eae0000f331f05783999dad6e1bef8ca933dec
HawkeyesWind <hawkeyes0@hotmail.com>
2019-02-03 22:20:31 +0800

    修改日志输出

--- autoReply.py
+++ autoReply.py
@@ -1,10 +1,20 @@
-import itchat, re, datetime, io
+import itchat, re, datetime, io, logging
 from itchat.content import TEXT
 from datetime import datetime
 
+FORMAT = "[%(asctime)-15s] [%(levelname)-8s] %(message)s"
+
 # 日志文件
-f = open("msg.log", "a", encoding="utf-8")
-print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')}] start", file=f, flush=True)
+handler = logging.FileHandler("msg.log", encoding="UTF-8")
+handler.formatter = logging.Formatter(FORMAT)
+logging.root.addHandler(handler)
+
+handler = logging.StreamHandler()
+handler.formatter = logging.Formatter(FORMAT)
+logging.root.addHandler(handler)
+
+logging.root.setLevel(logging.INFO)
+logging.info('start')
 
 # 只处理文本信息
 @itchat.msg_register([TEXT])
@@ -12,20 +22,19 @@ def text_reply(msg):
     ''' 消息处理函数 '''
     # 正则匹配消息文本
     match = re.search('年|春', msg['Text'])
-    # 获知发送人信息
-    user = itchat.search_friends(userName=msg['FromUserName'])
-    if match:
+    # 排除自己发的内容和腾讯新闻发的内容
+    if itchat.originInstance.storageClass.userName!=msg.FromUserName and msg.FromUserName != "newsapp" and match:
         # 自动回复
-        itchat.send(('新春快乐！'), msg['FromUserName'])
+        msg.user.send('新春快乐！')
         # 记录日志
-        log = f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')}] {user['NickName']}"
-        if user['RemarkName']:
-            log += "(" + user['RemarkName'] + ")"
-        log += ": " + msg['Text']
-        print(log, file=f, flush=True)
-        print(log)
+        log = f"{msg.user.NickName}"
+        if msg.user.RemarkName:
+            log += f"({msg.user.RemarkName})"
+        log += f": {msg.Text}"
+        # print(log, file=f, flush=True)
+        # print(log)
+        logging.info(log)
 
 # 自动登陆
 itchat.auto_login(hotReload=True)
 itchat.run()
-f.close()
